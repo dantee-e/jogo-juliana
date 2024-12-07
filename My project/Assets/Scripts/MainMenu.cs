5a433c0f48d058cf0b1282977455081dd0969954
dantee-e <ramacciotti.dante@gmail.com>
2024-12-07 01:51:17 -0300

    game manager comecou a tomar forma

--- My project/Assets/Scripts/MainMenu.cs
+++ My project/Assets/Scripts/MainMenu.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class MainMenu : MonoBehaviour
-{
+public class MainMenu : MonoBehaviour{
     public UnityEvent sinalJogar;
     public UnityEvent sinalSair;
 
-    public void sendSinal(string sinal)
-    {
-        switch(sinal){
+    public void sendSinal(string sinal){
+        Debug.Log("Received signal: " + sinal); // Debug message to see if the signal is being passed
+        switch (sinal){
             case "jogar":
                 sinalJogar?.Invoke();
                 break;
@@ -20,12 +19,12 @@ public class MainMenu : MonoBehaviour
         }
     }
 
-    public void Jogar() {
+    public void Jogar(){
         sendSinal("jogar");
     }
 
     // Fechar o jogo
-    public void Sair() {
+    public void Sair(){
         sendSinal("sair");
     }
 }
